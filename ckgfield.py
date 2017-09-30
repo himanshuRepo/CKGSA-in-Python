b0bdfe456c3913bc300835d85cff268d1b80cb1d
Himanshu Mittal <himanshu.mittal224@gmail.com>
2017-10-01 00:38:42 +0530

    Update ckgfield.py

--- ckgfield.py
+++ ckgfield.py
@@ -4,7 +4,7 @@ Created on Mon May  11 21:20:52 2017
 
 This is the code for Chaotic Kbest Gravitational Search Algorithm (CKGSA)
 Coded by: Himanshu Mittal, himanshu.mittal224@gmail.com
-The code template used is similar to code given at link: https://github.com/7ossam81/EvoloPy and matlab version of GSA at mathworks.
+The code template used is similar to code given at link: https://github.com/himanshuRepo/GSA_PythonCode and matlab version of GSA at mathworks.
 
 Reference: "H. Mittal,R. Pal,A. Kulhari,and M. Saraswat, 
             “Chaotic Kbest gravitational search algorithm (CKGSA)”,
