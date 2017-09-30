c4438e4f594c7f167154f95de760ed9384dc50ed
Himanshu Mittal <himanshu.mittal224@gmail.com>
2017-10-01 00:22:47 +0530

    Update massCalculation.py

--- massCalculation.py
+++ massCalculation.py
@@ -3,7 +3,9 @@
 Created on Mon May  1 21:20:52 2017
 
 This is the code for Chaotic Kbest Gravitational Search Algorithm (CKGSA)
-@author: Himanshu Mittal, himanshu.mittal224@gmail.com
+Coded by: Himanshu Mittal, himanshu.mittal224@gmail.com
+The code template used is similar to code given at link: https://github.com/7ossam81/EvoloPy and matlab version of GSA at mathworks.
+
 
 Reference: "H. Mittal,R. Pal,A. Kulhari,and M. Saraswat, 
             “Chaotic Kbest gravitational search algorithm (CKGSA)”,
